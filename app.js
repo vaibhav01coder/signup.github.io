52ccda2e6d5ed6a6f0be438395131b893d17fa39
Vivek Vaibhav <94289919+vaibhav01coder@users.noreply.github.com>
2023-08-19 13:47:53 +0530

    Add files via upload
    done

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require ("express");
+const bodyParser = require("body-parser");
+const request= require("request")
+
+const app= express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/",function(req,res){
+
+    var firstName= req.body.fName;
+    var lastName= req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName)
+})
+
+app.listen(3000,function(){
+    console.log("running");
+})
+
+
+
+//api key
+// b3b0a0c6de1e80969bfefc699c1e5d06-us13
+
+// Id
+// 2a917459db
